0dc5f03f385685f175ec205c65ed3e0d48dc84ba
Eklavya28Sharma <eklavya28sharma@gmail.com>
2025-11-10 11:41:45 +0530

    Q1.c

--- Q1.c
+++ Q1.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <math.h>   
+int main() {
+    int num, original, remainder, digits = 0;
+    double result = 0.0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-int main(){
-    int number,count=0,armstrong,final=0,temp2;
-    int temp,temp_second;
-    printf("Enter a number : ");
-    scanf("%d",&number);
-    temp = number;
-    temp_second = number;
-    for (int i = 0; i <= temp; i++)
-    {
-        armstrong = temp%10;
-        temp= temp/10;
-        count+=1;
+    original = num; 
+
+     int temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        digits++;
     }
-    for (int i = 0; i <=temp_second; i++)
-    {
-        temp2 = temp_second%10;
-        final+=pow(temp2,count);
-        temp_second=temp_second/10;
+    temp = num;
+    while (temp != 0) {
+        remainder = temp % 10;                 
+        result += pow(remainder, digits);      
+        temp /= 10;                            
     }
-    if(final==number){
-        printf("it is an armstrong.");
-    }
-    else{
-        printf("it is not an armstrong.");
-    }
-    
-    return 0;
+    if ((int)result == original)
+        printf("%d is an Armstrong number.\n", original);
+    else
+        printf("%d is NOT an Armstrong number.\n", original);
+     return 0;
 }
